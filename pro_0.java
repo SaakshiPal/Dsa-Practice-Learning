2984adfecb587ff442ccdc652e2a168ab1ba582f
Saakshi Pal <saakshipal23@gmail.com>
2026-02-21 09:18:45 +0530

    new line sout added

--- pro_0.java
+++ pro_0.java
@@ -2,5 +2,6 @@ class Pro_0 {
     public static void main(String[] args) {
         System.out.println("Hello, World");
         System.out.println("Welcome to Java programming!");
+        System.out.println("Let's learn and have fun coding!");
     }
 }
