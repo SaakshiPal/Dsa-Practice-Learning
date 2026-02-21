24316894a7a293705d80a84c7e5f41f25552d5ea
Saakshi Pal <saakshipal23@gmail.com>
2026-02-21 10:08:05 +0530

    new switch problem added

---
+++ pro_3.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class pro_3 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter the operator which need to perform(+,-,/,%,*): ");
+       char operator= sc.next().charAt(0);
+       System.out.println("Enter the first number: ");
+       double num1=sc.nextDouble();
+       System.out.println("Enter the second the second number: ");
+       double num2=sc.nextDouble();
+       double result;
+       switch  (operator){
+           case '+':
+               result= num1+num2;
+               System.out.println("The sum of the two number is: " +result);
+               break;
+           case '-':
+               result= num1-num2;
+               System.out.println("The difference of the two number is: " +result);
+               break;
+           case '*':
+               result= num1*num2;
+               System.out.println("The product of the two number is: " +result);
+               break;
+           case '/':
+               result= num1/num2;
+               System.out.println("The division of the two number is: " +result);
+               break;
+           case '%':
+               result= num1%num2;
+               System.out.println("The remainder of the two number is: " +result);
+               break;
+           
+        default:
+            System.out.println("Invalid operator");
+            break;
+       }
+       sc.close();
+     
+    }   
+}
