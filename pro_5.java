e2d5ad1f0cc047b85702304d6b7cab421fc02181
Saakshi Pal <saakshipal23@gmail.com>
2026-02-23 07:44:48 +0530

    problem of pattern 1 added

---
+++ pro_5.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class pro_5 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the no of col: ");
+        int col=sc.nextInt();
+        System.out.print("Enter no of row: ");
+        int row=sc.nextInt();
+        for(int i=1;i<=row;i++){
+            for (int j=1;j<=col;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
